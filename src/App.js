93c4206f460ef319894e0a04134f5c78629fe56e
Sergey <primosudov@gmail.com>
2021-12-15 19:31:04 +0400

    pomodoro timer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
+import react from 'react'
 import './App.css';
 
+function checkNum(time) {
+  if(time < 10) {
+     time = `0${time}`
+     return time
+  }
+   return time;
+}
+
 function App() {
+  const [titele, setTitele] = react.useState('Lets go start pomodoro !!!')
+  const [timeLeft, setTimeLeft] = react.useState(25*60);
+  const intervalRef = react.useRef(null);
+  
+  function startTimer() {
+    setTitele('just do it !')
+    intervalRef.current = setInterval( () => {
+      if (timeLeft >= 1) return setTimeLeft(timeLeft => timeLeft - 1);
+
+      resetTimer();       
+    },1000)
+  }
+
+  function stopTimer() {
+    clearInterval(intervalRef.current);
+    setTitele('Keep it up !')
+  }
+
+  function resetTimer() {
+    clearInterval(intervalRef.current);
+    setTimeLeft(25*60);
+    setTitele('next round ?')
+  }
+
+  const minites = checkNum(Math.floor(timeLeft / 60));
+  const seconds = checkNum(timeLeft - minites * 60);
+
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>{titele}</h2>
+
+      <div className='timer'>
+        <span>{minites}</span>
+        <span>:</span>
+        <span>{seconds}</span>
+      </div>
+
+      <div className='button'>
+        <button onClick={startTimer}>Start</button>
+        <button onClick={stopTimer}>Stop</button>
+        <button onClick={resetTimer}>Reset</button>
+      </div>
     </div>
   );
 }
